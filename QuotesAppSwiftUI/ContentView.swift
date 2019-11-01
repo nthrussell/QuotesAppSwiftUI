3c7af057417b231b7cebc62ad5bcbe923049b8dd
M A Russell <rredxx@icloud.com>
2019-11-01 10:06:16 +0600

    Initial Commit

---
+++ QuotesAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuotesAppSwiftUI
+//
+//  Created by Minhajul Russell on 11/1/19.
+//  Copyright © 2019 russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
